e90c75bc26b62df811aa60cb0f28827471b89a60
dylandupasquier <dylandupasquier@gmail.com>
2022-03-17 17:00:02 -0700

    added Morgan's suggestions

--- recreating-react-components-from-scratch/back-end/config/application.rb
+++ recreating-react-components-from-scratch/back-end/config/application.rb
@@ -6,6 +6,7 @@ Bundler.require(*Rails.groups)
 
 module BackEnd
   class Application < Rails::Application
+    config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     config.action_dispatch.cookies_same_site_protection = :strict
