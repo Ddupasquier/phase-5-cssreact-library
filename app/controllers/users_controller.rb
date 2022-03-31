510b1a33b17cc62b36ff2eb94742455d69329a77
dylandupasquier <dylandupasquier@gmail.com>
2022-03-31 10:08:13 -0700

    status changed to unprocessable entity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   private
 
   def render_unprocessable_entity(invalid)
-      render json:{error: invalid.record.errors}, status: :render_unprocessable_entity
+      render json:{error: invalid.record.errors}, status: :unprocessable_entity
   end
 
   def user_params
